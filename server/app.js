a427c69313b5a01befd8a606a56e47260dc64bb3
Aayush Chhabra <aayush@Aayushs-MacBook-Pro.local>
2021-09-27 21:49:48 -0700

    Mmongo models for book and author

--- server/app.js
+++ server/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect("mongodb+srv://aayush:test123@gql-0.s25gr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongo');
+})
+
 app.use('/graphql', graphqlHTTP({
-    schema
+    schema,
+    graphiql: true
 }));
 
 app.listen(4000, ()=>{
